77735c8a628f851318bb863159e1f0c02fc2c81f
Jesse Tomchak <jtomchak@gmail.com>
2017-11-07 18:38:12 -0700

    bit of formating

--- src/Main.elm
+++ src/Main.elm
@@ -10,6 +10,7 @@ import Random
 
 ---- MODEL ----
 
+
 type alias Photo =
     { url : String }
 
@@ -80,41 +81,41 @@ update msg model =
         _ =
             Debug.log "Message Data" msg
     in
-    --case expression
-    case msg of
-        SelectByUrl url ->
-            ( { model | selectedUrl = Just url }, Cmd.none )
+        --case expression
+        case msg of
+            SelectByUrl url ->
+                ( { model | selectedUrl = Just url }, Cmd.none )
 
-        SurpriseMe ->
-            let
-                randomPhotoPicker : Random.Generator Int
-                randomPhotoPicker =
-                    Random.int 0 (List.length model.photos - 1)
-            in
-            ( model, Random.generate SelectByIndex randomPhotoPicker )
+            SurpriseMe ->
+                let
+                    randomPhotoPicker : Random.Generator Int
+                    randomPhotoPicker =
+                        Random.int 0 (List.length model.photos - 1)
+                in
+                    ( model, Random.generate SelectByIndex randomPhotoPicker )
 
-        SetSize size ->
-            ( { model | chosenSize = size }, Cmd.none )
+            SetSize size ->
+                ( { model | chosenSize = size }, Cmd.none )
 
-        SelectByIndex index ->
-            let
-                newSelectedUrl : Maybe String
-                newSelectedUrl =
-                    model.photos
-                        |> Array.fromList
-                        |> Array.get index
-                        |> Maybe.map .url
-            in
-            ( { model | selectedUrl = newSelectedUrl }, Cmd.none )
+            SelectByIndex index ->
+                let
+                    newSelectedUrl : Maybe String
+                    newSelectedUrl =
+                        model.photos
+                            |> Array.fromList
+                            |> Array.get index
+                            |> Maybe.map .url
+                in
+                    ( { model | selectedUrl = newSelectedUrl }, Cmd.none )
 
-        LoadPhotos (Ok responseStr) ->
-                    let
-                        urls =
-                            String.split "," responseStr
+            LoadPhotos (Ok responseStr) ->
+                let
+                    urls =
+                        String.split "," responseStr
 
-                        photos =
-                            List.map Photo urls
-                    in
+                    photos =
+                        List.map Photo urls
+                in
                     ( { model
                         | photos = photos
                         , selectedUrl = List.head urls
@@ -122,8 +123,8 @@ update msg model =
                     , Cmd.none
                     )
 
-        LoadPhotos (Err _) ->
-            ( { model | loadingError = Just "Error! (Try turning it off and on again?)"}, Cmd.none)
+            LoadPhotos (Err _) ->
+                ( { model | loadingError = Just "Error! (Try turning it off and on again?)" }, Cmd.none )
 
 
 
@@ -145,7 +146,6 @@ view model =
         , h3 [] [ text "Thumbnail Size:" ]
         , div [ id "choose-size" ]
             (List.map (viewSizeChoose model.chosenSize) [ Small, Medium, Large ])
-
         , div [ id "thumbnails", class (sizeToString model.chosenSize) ]
             (List.map
                 (viewThumbnail model.selectedUrl)
@@ -167,8 +167,6 @@ viewLarge maybeUrl =
                 , src (urlPrefix ++ "large/" ++ url)
                 ]
                 []
-
-
 
 
 viewThumbnail : Maybe String -> Photo -> Html Msg
@@ -198,9 +196,6 @@ viewSizeChoose chosenSize size =
         ]
 
 
-
-
-
 sizeToString : ThumbnailSize -> String
 sizeToString size =
     case size of
@@ -213,18 +208,20 @@ sizeToString size =
         Large ->
             "large"
 
+
 viewOrError : Model -> Html Msg
-viewOrError model = 
+viewOrError model =
     case model.loadingError of
         Nothing ->
             view model
-            
+
         Just errorMessage ->
-            div [ class "error-message"]
-                [ h1 [] [text "Photo Groove"] 
-                , p [] [text errorMessage]
+            div [ class "error-message" ]
+                [ h1 [] [ text "Photo Groove" ]
+                , p [] [ text errorMessage ]
                 ]
-            
+
+
 
 ---- PROGRAM ----
 
